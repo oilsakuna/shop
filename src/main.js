dce2ecc6807758392e0522464d2c1f1e7edae7bd
oilsakuna <75597215+oilsakuna@users.noreply.github.com>
2021-02-22 22:46:33 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp} from 'vue'
+import App from './App.vue'
+import router from './router'
+
+
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Vue.use(BootstrapVue)
+// Vue.config.productionTip = false
+
+
+createApp(App).use(router).mount('#app')
+
